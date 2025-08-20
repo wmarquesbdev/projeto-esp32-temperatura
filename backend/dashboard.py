5b1e92da6569a7ebfde5620cba12ef928ec11b96
William Marques Barbosa <202302319068@alunos.estacio.br>
2025-08-20 11:33:35 -0400

    Resolve conflitos de bibliotecas python

--- backend/dashboard.py
+++ backend/dashboard.py
@@ -4,7 +4,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 from datetime import datetime, timedelta
 import numpy as np
-
+    
 try:
     from config import current_config
     errors = current_config.validar_configuracao()
